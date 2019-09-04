21507b4912b8d8269f7aeed6a0b6a785aa430c7b
Rasmus Larsen <ralars@dtu.dk>
2019-09-04 13:38:12 +0200

    trying stuff for ex01

--- irlc_tools/ex01/ex01_1_1.py
+++ irlc_tools/ex01/ex01_1_1.py
@@ -1,13 +1,49 @@
-from irlc_tools.resources import foobar
-print("Hello world. Such RL, Much control.")
+from typing import NamedTuple
+import numpy as np
 
-if __name__ == "__main__":
-    print("Exercize 1.1.1 is running...")
-    x = 43
+K = 4
+ACTIONS = {(1, 0), (-1, 0), (0, 1), (0, -1)}
+NX = 5
+NY = 5
+GOAL = (NX-1, NY-1)
+J = np.zeros([NX, NY])
 
-    print("x is")
-    print(x)
-    # please insert cutout code here.
-    # your code done now.
+
+class GridworldState(NamedTuple):
+    x: int
+    y: int
+
+
+def clip(val, lower, upper):
+    return max(lower, min(upper, val))
+
+
+def step(state: GridworldState, action):
+    assert action in ACTIONS
+
+    if (state.x, state.y) == GOAL:
+        cost = 0.
+        return state, cost
+    cost = 1.
+    x = clip(state.x + action[0], 0, NX-1)
+    y = clip(state.y + action[1], 0, NY-1)
+
+    return GridworldState(x, y), cost
+
+
+for k in range(K):
+    V = J.copy()
+    for x in range(NX):
+        for y in range(NY):
+            best = np.inf
+            for a in ACTIONS:
+                s, g = step(GridworldState(x, y), a)
+                val = g + J[s.x, s.y]
+                if val < best:
+                    best = val
+                    V[x, y] = val
+    J = V
+    print(J)
+
 
 
