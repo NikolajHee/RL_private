81611d23df9f353a0d214632185cf4dd20369354
Tue Herlau <tuhe@dtu.dk>
2022-05-22 15:40:56 +0200

    Fixed bad import for MC estimation

--- irlc/exam_tabular_examples/lecture_10_mc_value_every.py
+++ irlc/exam_tabular_examples/lecture_10_mc_value_every.py
@@ -1,5 +1,5 @@
 # This file may not be shared/redistributed without permission. Please read copyright notice in the git repo. If this file contains other copyright notices disregard this text.
-from irlc.lectures.lec10.lecture_10_mc_q_estimation import keyboard_play_value
+from irlc.exam_tabular_examples.helper import keyboard_play_value
 from irlc.gridworld.gridworld_environments import BookGridEnvironment
 from irlc.ex10.mc_evaluate import MCEvaluationAgent
 
